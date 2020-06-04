bfc4a7d0825b5fee4e5641f7ba5c6b301e60294c
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-06-04 16:47:08 +0800

    提交代码

--- src/test/java/test/study/springboot/shiro/jwt/JwtUtilsTest.java
+++ src/test/java/test/study/springboot/shiro/jwt/JwtUtilsTest.java
@@ -13,14 +13,15 @@ public class JwtUtilsTest {
     @Test
     public void test() {
         Map<String, String> claims = Maps.newHashMap();
+        claims.put("iss", "asdfasdf");
         claims.put("username", "tom");
         claims.put("password", "123456");
 
-        String jwtToken = JwtUtils.createJwt(claims);
-        log.info("{}", jwtToken);
+        String jwt = JwtUtils.createJwt(claims);
+        log.info("{}", jwt);
 
-        log.info("合法=" + JwtUtils.verifyJwt(jwtToken));
-        JwtUtils.parseJwt(jwtToken).forEach((k, v) -> {
+        log.info("合法 {}", JwtUtils.verifyJwt(jwt));
+        JwtUtils.parseJwt(jwt).forEach((k, v) -> {
             log.info("{}= {}", k, v);
         });
     }
