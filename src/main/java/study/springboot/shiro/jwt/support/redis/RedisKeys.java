fcba98e406063124ef64438e8f934134c0eb7e6b
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-06-04 14:31:05 +0800

    提交代码

--- src/main/java/study/springboot/shiro/jwt/support/redis/RedisKeys.java
+++ src/main/java/study/springboot/shiro/jwt/support/redis/RedisKeys.java
@@ -6,7 +6,7 @@ public class RedisKeys {
 
     private static final String SEPARATOR = ":";
 
-    private static final String APP = "shiro";
+    private static final String APP = "shiro:jwt";
 
     private static String format(String... key) {
         return Joiner.on(SEPARATOR).skipNulls().join(key);
@@ -14,9 +14,5 @@ public class RedisKeys {
 
     public static String keyOfToken(String token) {
         return format(APP, "token", token);
-    }
-
-    public static String keyOfUserInfo(String email) {
-        return format(APP, "usr_info", email);
     }
 }
